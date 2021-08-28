a921b841de6c001f3d8bcb00b97d751324d6d571
mijan <mejan601@gmail.com>
2021-08-28 13:44:25 +0600

    flask-pymongo include and server change

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from os import name
+from bson import objectid
 from flask import Flask  , render_template , request , redirect, flash, jsonify
 from flask.helpers import make_response
 from flask_login import LoginManager, logout_user, login_required, login_user, current_user, UserMixin, COOKIE_NAME, COOKIE_SECURE
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from bson.json_util import loads, dumps
 from modules import *
 import time
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = ami["Username1"]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/login.db"
-app.config["SQLALCHEMY_BINDS"] = {"daily":"sqlite:///database/daily.db",
-                                    "experience" : "sqlite:///database/experience.db",
-                                    "favourite" : "sqlite:///database/favorite.db"}
+app.config["MONGO_URI"] = f"mongodb+srv://{ami['username3']}:{ami['password']}@mejan-cluster.1ce4z.mongodb.net/ideaex"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 
 login_manager = LoginManager()
@@ -30,45 +30,6 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-class blog_post(db.Model):
-    __bind_key__ = "daily"
-    sno = db.Column(db.Integer , primary_key=True)
-    name = db.Column(db.String(200))
-    subject = db.Column(db.String(200))
-    post = db.Column(db.String(1000))
-    effective = db.Column(db.String(5))
-    time_ = db.Column(db.String(30))
-
-
-class Experience(db.Model):
-    __bind_key__ = "experience"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    item = db.Column(db.String(50))
-    heading = db.Column(db.String(100))
-    description = db.Column(db.String(200))
-    writing = db.Column(db.String(1000))
-    select = db.Column(db.String(5))
-    time_ = db.Column(db.String(30))
-
-class Favourite(db.Model):
-    __bind_key__ = "favourite"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    person = db.Column(db.String(200))
-    drink = db.Column(db.String(200))
-    animal = db.Column(db.String(200))
-    flower = db.Column(db.String(200))
-    place = db.Column(db.String(200))
-    fruit = db.Column(db.String(200))
-    fish = db.Column(db.String(200))
-    food = db.Column(db.String(200))
-    game = db.Column(db.String(200))
-    movie = db.Column(db.String(200))
-    tvshow = db.Column(db.String(200))
-    select = db.Column(db.String(5))
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -91,11 +52,13 @@ def login():
 @app.route("/")
 @login_required
 def daily_dairies():
-    favourites = Favourite.query.filter_by(name=current_user.name).first()
+    favourites = loads(dumps(mongo.db.favourites.find_one({'name':current_user.name}))) 
     if not favourites:
         return redirect("/favourite")
-    dairies = blog_post.query.all()
-    experiences = Experience.query.all()
+    dairies = mongo.db.ideas.find()
+    dairies = loads(dumps(dairies))
+    experiences = mongo.db.experiences.find()
+    experiences = loads(dumps(experiences))
     dairies.reverse()
     experiences.reverse()
     return render_template("main.html", dairies=dairies, experiences=experiences)
@@ -115,32 +78,29 @@ def blog_post_():
         post = request.form["post"]
         effective = request.form["select"]
         if post and address:
-            post_blog = blog_post(name=current_user.name,subject=address,
-             post=post, effective=effective, time_=time.asctime())
-            db.session.add(post_blog)
-            db.session.commit()
+            mongo.db.ideas.insert_one({"name":current_user.name,"subject":address,
+             "post":post, "effective":effective, "time_":time.asctime()})
         return redirect("/daily_diary")
 
-    posts = blog_post.query.filter_by(name=current_user.name).all()
+    posts = loads(dumps(mongo.db.ideas.find()))
     posts.reverse()
     response = make_response(render_template("dist/daily.html" , posts=posts, user=current_user.name, foo=42))
     response.set_cookie("Daily", "People read this diary")
     return response
 
-@app.route("/daily_edit/<int:sno>", methods=["GET", "POST"])
+@app.route("/daily_edit/<string:_id>", methods=["GET", "POST"])
 @login_required
-def daily_edit(sno):
+def daily_edit(_id):
+    print("okay")
     if request.method == "POST":
         subject = request.form["address"]
         post = request.form["post"]
-        before_edit = blog_post.query.filter_by(sno=sno).first()
-        before_edit.subject = subject
-        before_edit.post = post
-        db.session.add(before_edit)
-        db.session.commit()
+        mongo.db.ideas.find_one_and_update({"_id":objectid.ObjectId(_id)},{"$set":{"subject":subject, "post":post}})
         return redirect("/daily_diary")
 
-    post = blog_post.query.filter_by(sno=sno).first()
+    print("okay")
+    post = loads(dumps(mongo.db.ideas.find_one({"_id":objectid.ObjectId(_id)})))
+    print(post)
     response = make_response(render_template("dist/daily_edit.html", post=post))
     response.set_cookie("Edit_diary", "Edit your diary")
     return response
@@ -155,43 +115,35 @@ def experience_():
         description=request.form["description"]
         writing =request.form["writing"]
         select = request.form["select"]
-        experience_save = Experience(name=current_user.name, item=item, heading=heading,
-         description=description, writing=writing, select=select, time_=time.asctime())
-        db.session.add(experience_save)
-        db.session.commit()
+        mongo.db.experiences.insert_one({"name":current_user.name, "item":item, "heading":heading,
+         "description":description, "writing":writing, "select":select,"time_":time.asctime()})
         return redirect("/experience")
 
-    experiences = Experience.query.filter_by(name=current_user.name).all()
+    experiences = loads(dumps(mongo.db.experiences.find({"name":current_user.name})))
     experiences.reverse()
     response = make_response(render_template("dist/experience.html", experiences=experiences))
     response.set_cookie("Experience", "have")
     return response
 
-@app.route("/experience_edit/<int:id>", methods=["GET", "POST"])
+@app.route("/experience_edit/<string:_id>", methods=["GET", "POST"])
 @login_required
-def experience_edit(id):
+def experience_edit(_id):
     if request.method == "POST":
         item = request.form["item"]
         heading = request.form["heading"]
         description= request.form["description"]
         writing = request.form["writing"]
-        before_edit = Experience.query.filter_by(id=id).first()
-        before_edit.item = item
-        before_edit.heading = heading
-        before_edit.description = description
-        before_edit.writing = writing
-        db.session.add(before_edit)
-        db.session.commit()
+        mongo.db.experiences.find_one_and_update({'_id':objectid.ObjectId(_id)}, {"$set":{"item":item,"heading":heading, "description":description, "writing":writing}})
         return redirect("/experience")
 
-    post = Experience.query.filter_by(id=id).first()
-    print(post)
+    post = loads(dumps(mongo.db.experiences.find_one({'_id':objectid.ObjectId(_id)})))
     return render_template("dist/experience_edit.html", experience=post, user=current_user.name)
 
 @app.route("/favourite", methods=["GET", "POST"])
 @login_required
 def favourite():
-    favourites = Favourite.query.filter_by(name=current_user.name).first()
+    favourites =loads(dumps(mongo.db.favourites.find_one({'name':current_user.name})))
+    print(favourites)
     if favourites:
         return render_template("dist/favourite.html", favourites=favourites, user=current_user.name, time=time.asctime())
     elif request.method == "POST":
@@ -206,10 +158,8 @@ def favourite():
         movie =request.form["movie"]
         tvshow =request.form["tvshow"]
         select = request.form["select"]
-        save_sql = Favourite(name=current_user.name,person=person,fish=fish,place=place,flower=flower,
-        animal=animal,drink=drink,food=food,game=game,movie=movie,tvshow=tvshow, select=select)
-        db.session.add(save_sql)
-        db.session.commit()
+        mongo.db.favourites.insert_one({'name':current_user.name,"person":person,'fish':fish,'place':place,'flower':flower,
+        'animal':animal,'drink':drink,'food':food,'game':game,'movie':movie,'tvshow':tvshow, 'select':select})
         return redirect("/favourite")
         
     return render_template("dist/favourite_set.html")
--- modules/constant_variable.py
+++ modules/constant_variable.py
@@ -1,5 +1,7 @@
 
 
 ami = {"Username1":"Have1nice2day#",
-"Username2":"hangover"}
+"Username2":"hangover",
+"password":"KdgEkDGQ8rPpjNS4",
+"username3":"ideaex_fav"}
 
